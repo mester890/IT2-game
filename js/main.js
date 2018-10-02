5518292046e1e31e6b8be98e1f06baa4de611fb9
mester890 <mester890@users.noreply.github.com>
2018-10-02 10:46:34 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const output = document.body.querySelector('#output');
+
+function startGame() {
+  output.innerHTML = `
+  <p>
+    Du er strandet på en øde øy. Du må ta de riktige valgene for å komme deg til spillets ende. Greier du utfordringen?
+  </p>
+  <button type="button" name="button" onclick="stageOne('yes');">Ja!</button>
+  <button type="button" name="button" onclick="stageOne('no');">Nei</button>
+  `;
+
+}
+
+function stageOne(choice) {
+  if(choice == 'yes') {
+    moveOn();
+  } else if (choice == 'no') {
+    endGame('Du hadde ikke det som skulle til for å gå videre i spillet. Da avslutter vi med en gang...');
+  }
+}
+
+function endGame(reason) {
+  output.innerHTML = `
+    <p>Ajjj... ${reason}</p>
+    <br>
+    <p>Prøve igjen?</p>
+    <button type="button" onclick="startGame()">Ja!</button>
+  `
+}
